39feea39e27a4e7832a07ef6ee1bb1f0faf2d8c0
Leandro Mangini Antunes <leandrowkz@gmail.com>
2023-08-20 16:56:03 +0100

    feat: add bookcarousel basics

--- src/components/BookCarousel/BookCarousel.styles.ts
+++ src/components/BookCarousel/BookCarousel.styles.ts
@@ -2,8 +2,18 @@ import { createStyles } from '@mantine/core';
 
 export const useStyles = createStyles((theme) => ({
   carousel: {
-    // marginLeft: `calc(${theme.spacing.xl} * -1)`,
-    // paddingLeft: theme.spacing.xl,
-    maxWidth: `calc(100vw)`
+    '& .mantine-Carousel-viewport': {
+      paddingLeft: theme.spacing.xl
+    },
+    '& .mantine-Carousel-controls': {
+      paddingLeft: `calc(${theme.spacing.lg} * 1.8)`
+    }
+  },
+  cover: {
+    aspectRatio: '1.5/1'
+  },
+  wrapper: {
+    marginLeft: `calc(${theme.spacing.xl} * -1)`,
+    width: `calc(100% + calc(${theme.spacing.xl} * 2))`
   }
 }));
--- src/components/BookCarousel/BookCarousel.tsx
+++ src/components/BookCarousel/BookCarousel.tsx
@@ -1,21 +1,35 @@
 import { Carousel, CarouselProps } from "@mantine/carousel";
-import { Image } from "@mantine/core";
+import { Box, Image, useMantineTheme } from "@mantine/core";
 import { useStyles } from "./BookCarousel.styles";
+import { useMediaQuery } from "@mantine/hooks";
 
 export type BookCarouselProps = CarouselProps & {
   books: any[]
 }
 
 export function BookCarousel({ books, ...props }: BookCarouselProps) {
+  const theme = useMantineTheme();
+  const mobile = useMediaQuery(`(max-width: ${theme.breakpoints.sm})`);
   const { classes } = useStyles()
 
   return (
-    <Carousel {...props} slideSize="200px" slideGap="lg" align="start" controlSize={40} className={classes.carousel}>
-      {books.map((book) => (
-        <Carousel.Slide key={`book-${book.ISBN}`}>
-          <Image src={book.image} radius="md" fit="cover" withPlaceholder height={300} width={200} />
-        </Carousel.Slide>
-      ))}
-    </Carousel>
+    <Box className={classes.wrapper}>
+      <Carousel
+        {...props}
+        slideSize="15%"
+        breakpoints={[{ maxWidth: 'sm', slideSize: '100%', slideGap: 2 }]}
+        slideGap="lg"
+        align="start"
+        slidesToScroll={mobile ? 1 : undefined}
+        controlSize={40}
+        className={classes.carousel}
+      >
+        {books.map((book) => (
+          <Carousel.Slide key={`book-${book.ISBN}`}>
+            <Image src={book.image} radius="md" fit="cover" withPlaceholder width="100%" className={classes.cover} />
+          </Carousel.Slide>
+        ))}
+      </Carousel>
+    </Box>
   )
 }
