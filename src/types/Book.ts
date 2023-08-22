33bb7ec1aeb7ecb84a38a16f1f896a2cff602173
Leandro Mangini Antunes <leandrowkz@gmail.com>
2023-08-22 17:04:04 +0100

    feat: add initial types

---
+++ src/types/Book.ts
@@ -0,0 +1,17 @@
+import type { Author } from './Author'
+import type { BookMetadata } from './BookMetadata'
+
+export type Book = {
+  id: string
+  isbn: string[]
+  title: string
+  description: Nullable<string>
+  authors: Author[]
+  categories: string[]
+  cover: Nullable<string>
+  publisher: Nullable<string>
+  publishedAt: Nullable<string>
+  pageCount: Nullable<number>
+  language: LanguageCode
+  metadata: BookMetadata
+}
---
+++ src/types/BookMetadata.ts
@@ -0,0 +1,5 @@
+export type BookMetadata = {
+  isEbook: boolean
+  isMature: boolean
+  isPublicDomain: boolean
+}
