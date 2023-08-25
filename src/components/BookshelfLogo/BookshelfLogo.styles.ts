184db1a7254eff1c7ca7db85fd88da224a0ee9e3
Leandro Mangini Antunes <leandrowkz@gmail.com>
2023-08-25 21:37:22 +0100

    feat: add new logo

--- src/components/BookshelfLogo/BookshelfLogo.styles.ts
+++ src/components/BookshelfLogo/BookshelfLogo.styles.ts
@@ -2,7 +2,7 @@ import { createStyles } from '@mantine/core'
 
 export const useStyles = createStyles((theme) => ({
   link: {
-    '&, &:visited': {
+    '&, &:visited, &:hover': {
       color: theme.colors.violet[9],
       textDecoration: 'none',
     },
--- src/components/BookshelfLogo/BookshelfLogo.tsx
+++ src/components/BookshelfLogo/BookshelfLogo.tsx
@@ -1,13 +1,18 @@
-import { Title } from '@mantine/core'
-import Link from 'next/link'
+import { Anchor, Flex, Text } from '@mantine/core'
 import { useStyles } from './BookshelfLogo.styles'
+import { IconBooks } from '@tabler/icons-react'
 
 export function BookshelfLogo() {
   const { classes } = useStyles()
 
   return (
-    <Link href="/" className={classes.link}>
-      <Title order={1}>📓 bookshelf</Title>
-    </Link>
+    <Anchor href="/" className={classes.link} data-testid="logo">
+      <Text weight={700} size="xl">
+        <Flex align="center" gap="sm">
+          <IconBooks />
+          Bookshelf
+        </Flex>
+      </Text>
+    </Anchor>
   )
 }
