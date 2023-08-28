a840180dcd8f46e20b1e246d81944e5b18d1d448
Leandro Mangini Antunes <leandrowkz@gmail.com>
2023-08-28 21:53:15 +0100

    test: add new tmp config file for jest

--- jest.config.ts
+++ jest.config.ts
@@ -1,10 +1,51 @@
-import type { Config } from 'jest'
+// import type { Config } from 'jest'
 
-const config: Config = {
+// const config: Config = {
+//   preset: 'ts-jest',
+//   rootDir: '.',
+//   testEnvironment: 'jest-environment-jsdom',
+//   setupFilesAfterEnv: ['<rootDir>/test-utils/jest.setup.ts'],
+//   moduleNameMapper: {
+//     '@next/font/(.*)': require.resolve('next/dist/build/jest/__mocks__/nextFontMock.js'),
+//     '^@/test-utils/(.*)$': '<rootDir>/test-utils/$1',
+//     '^@/test-utils': '<rootDir>/test-utils/index.ts',
+//     '^@/(.*)$': '<rootDir>/src/$1',
+//   },
+//   transform: {
+//     '^.+\\.jsx$': 'babel-jest',
+//     '^.+\\.tsx?$': [
+//       'ts-jest',
+//       {
+//         tsconfig: 'tsconfig.tests.json',
+//       },
+//     ],
+//   },
+//   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+// }
+
+// export default config
+
+import nextJest from 'next/jest'
+
+const createJestConfig = nextJest({
+  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+  dir: './',
+  // moduleNameMapper: {
+  //   // '@next/font/(.*)': require.resolve('next/dist/build/jest/__mocks__/nextFontMock.js'),
+  //   '^@/test-utils/(.*)$': '<rootDir>/test-utils/$1',
+  //   '^@/test-utils': '<rootDir>/test-utils/index.ts',
+  //   '^@/(.*)$': '<rootDir>/src/$1',
+  // },
+})
+
+// Add any custom config to be passed to Jest
+/** @type {import('jest').Config} */
+const config = {
+  // Add more setup options before each test is run
+  setupFilesAfterEnv: ['<rootDir>/test-utils/jest.setup.ts'],
   preset: 'ts-jest',
   rootDir: '.',
   testEnvironment: 'jest-environment-jsdom',
-  setupFilesAfterEnv: ['<rootDir>/test-utils/jest.setup.ts'],
   moduleNameMapper: {
     '^@/test-utils/(.*)$': '<rootDir>/test-utils/$1',
     '^@/test-utils': '<rootDir>/test-utils/index.ts',
@@ -22,4 +63,5 @@ const config: Config = {
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
 }
 
-export default config
+// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
+export default createJestConfig(config)
