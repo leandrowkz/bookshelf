e85064e36c8da8a944ddf6a7453a7b16fd34df7c
Leandro Mangini Antunes <leandrowkz@gmail.com>
2023-08-30 21:28:15 +0100

    refactor: update header links/styles

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,28 +1,31 @@
-// import { useState } from 'react';
 import { Header as MantineHeader, Group, Burger, Button, Box } from '@mantine/core'
 import { useDisclosure } from '@mantine/hooks'
 import { useStyles } from './Header.styles'
-import Link from 'next/link'
 import { BookshelfLogo } from '@/components/BookshelfLogo/BookshelfLogo'
 import { ColorSchemeToggle } from '../ColorSchemaToggle/ColorSchemaToggle'
 import { useMenuLinks } from '@/hooks/useMenuLinks'
+import { useRouter, usePathname } from 'next/navigation'
 
 export function Header() {
+  const router = useRouter()
+  const pathname = usePathname()
   const { header } = useMenuLinks()
   const [opened, { toggle }] = useDisclosure(false)
-  // const [active, setActive] = useState(header[0].link);
-  const { classes, cx, theme } = useStyles()
+  const { classes, theme } = useStyles()
 
-  const items = header.map((link) => (
-    <Link
-      key={link.label}
-      href={link.link}
-      className={cx(classes.link)}
-      // className={cx(classes.link, { [classes.linkActive]: active === link.link })}
-    >
-      {link.label}
-    </Link>
-  ))
+  const items = header.map((link) => {
+    const isActive = pathname === link.link
+
+    return (
+      <Button
+        variant={isActive ? 'light' : 'subtle'}
+        key={link.label}
+        onClick={() => router.push(link.link)}
+      >
+        {link.label}
+      </Button>
+    )
+  })
 
   return (
     <MantineHeader height={theme.other.headerHeight} className={classes.mantineHeader}>
@@ -36,8 +39,10 @@ export function Header() {
         </Group>
 
         <Group className={classes.social} position="right" noWrap>
-          <Button variant="subtle">Sign in</Button>
-          <Button>Sign up</Button>
+          <Button onClick={() => router.push('/sign-in')} variant="subtle">
+            Sign in
+          </Button>
+          <Button onClick={() => router.push('/sign-up')}>Sign up</Button>
           <ColorSchemeToggle size="xl" />
         </Group>
       </Box>
