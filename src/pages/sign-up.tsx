c7bdaa846b231b256bfb3f73df5989834ec9431d
Leandro Mangini Antunes <leandrowkz@gmail.com>
2023-08-30 21:38:27 +0100

    feat: sign up page shell

--- src/pages/sign-up.tsx
+++ src/pages/sign-up.tsx
@@ -1,49 +1,53 @@
-import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
-// import { useRouter } from 'next/navigation'
-import { useState } from 'react'
+import { Layout } from '@/components'
+import {
+  TextInput,
+  PasswordInput,
+  Anchor,
+  Paper,
+  Title,
+  Text,
+  Container,
+  Group,
+  Button,
+  Divider,
+} from '@mantine/core'
+import { IconBrandGoogle } from '@tabler/icons-react'
 
 export default function Page() {
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  // const router = useRouter()
-  const supabase = createClientComponentClient()
-
-  const handleSignUp = async () => {
-    await supabase.auth.signUp({
-      email,
-      password,
-      options: {
-        emailRedirectTo: `${location.origin}/auth/callback`,
-      },
-    })
-    // router.refresh()
-  }
-
-  const handleSignIn = async () => {
-    await supabase.auth.signInWithPassword({
-      email,
-      password,
-    })
-    // router.refresh()
-  }
-
-  const handleSignOut = async () => {
-    await supabase.auth.signOut()
-    // router.refresh()
-  }
-
   return (
-    <>
-      <input name="email" onChange={(e) => setEmail(e.target.value)} value={email} />
-      <input
-        type="password"
-        name="password"
-        onChange={(e) => setPassword(e.target.value)}
-        value={password}
-      />
-      <button onClick={handleSignUp}>Sign up</button>
-      <button onClick={handleSignIn}>Sign in</button>
-      <button onClick={handleSignOut}>Sign out</button>
-    </>
+    <Layout>
+      <Container size={460} my={40}>
+        <Title
+          align="center"
+          sx={(theme) => ({ fontFamily: `Greycliff CF, ${theme.fontFamily}`, fontWeight: 900 })}
+        >
+          Create your account
+        </Title>
+        <Text color="dimmed" size="sm" align="center" mt={5}>
+          Already have an account?{' '}
+          <Anchor size="sm" href="/sign-in">
+            Sign in
+          </Anchor>
+        </Text>
+
+        <Paper withBorder shadow="md" p={30} mt="lg" radius="md">
+          <Group grow mb="md" mt="md">
+            <Button leftIcon={<IconBrandGoogle />} variant="default">
+              Sign up using with Google
+            </Button>
+          </Group>
+
+          <Divider label="Or continue with email" labelPosition="center" my="lg" />
+
+          <TextInput label="Name" placeholder="John Doe" required />
+          <TextInput label="Email" placeholder="you@mantine.dev" required mt="md" />
+          <PasswordInput label="Password" placeholder="Your password" required mt="md" />
+          <PasswordInput label="Confirm password" placeholder="Your password" required mt="md" />
+          <Button fullWidth mt="lg">
+            Sign up using email
+          </Button>
+        </Paper>
+      </Container>
+    </Layout>
   )
 }
