80e476f0bed5652648490234900cdb65b7e34a8c
Leandro Mangini Antunes <leandrowkz@gmail.com>
2023-09-01 11:57:49 +0100

    refactor: remove unecessary code from layout

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,25 +1,13 @@
-import { useEffect, type PropsWithChildren } from 'react'
+import { type PropsWithChildren } from 'react'
 import { Box } from '@mantine/core'
 import { Header } from '@/components/Header/Header'
 import { Footer } from '@/components/Footer/Footer'
 import { useStyles } from './Layout.styles'
-import { NextResponse } from 'next/server'
-import { useAuthUser } from '@/hooks/useAuthUser'
-import { useRouter } from 'next/router'
 
 export type LayoutProps = PropsWithChildren
 
 export function Layout({ children }: LayoutProps) {
   const { classes } = useStyles()
-
-  // const user = useAuthUser()
-  // const router = useRouter()
-
-  // useEffect(() => {
-  //   if (!user) {
-  //     router.push('/')
-  //   }
-  // }, [])
 
   return (
     <Box className={classes.page}>
